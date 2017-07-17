56a177dfa8e78630b79157ce241f04ef87887d72
palmierie <ericmpalmieri@gmail.com>
2017-07-17 16:30:37 -0500

    finished loops exercise

--- for-loops.js
+++ for-loops.js
@@ -1,1 +1,39 @@
 console.log('WORKS');
+
+//Increment by 10
+for (i=5;i<120;i+=10){
+  console.log('Increment-Current value is ', i);
+}
+//Decrement by division
+for (i=4096;i>1;i/=2){
+  console.log('Divide-Current value is', i); 
+}
+
+//Array of Presidents
+
+let presidents = ["George Washington", "John Adams", "Thomas Jefferson", "James Madison", "James Monroe", "John Quincy Adams", "Andrew Jackson", "Martin Van Buren", "William Henry Harrison", "John Tyler", "James K. Polk", "Zachary Taylor", "Millard Fillmore", "Franklin Pierce", "James Buchanan", "Abraham Lincoln", "Andrew Johnson", "Ulysses S. Grant", "Rutherford B. Hayes", "James A. Garfield", "Chester A. Arthur", "Grover Cleveland", "Benjamin Harrison", "Grover Cleveland", "William McKinley", "Theodore Roosevelt", "William Howard Taft", "Woodrow Wilson", "Warren G. Harding", "Calvin Coolidge", "Herber Hoover", "Franklin D. Roosevelt", "Harry Truman", "Dwight D. Eisenhower", "John F. Kennedy", "Lyndon B. Johnson", "Richard Nixon", "Gerald Ford", "Jimmy Carter", "Ronald Reagan", "George H. W. Bush", "Bill Clinton", "George W. Bush", "Barack Obama", "Donald Trump"]
+
+for (i = 0; i < presidents.length; i++) {
+   console.log('President '+ [i+1], presidents[i]);
+}
+
+//Iterate Over Objects
+
+var antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
+
+for (var prop in antSpecies){
+console.log(`antSpecies.${prop} = ${antSpecies[prop]}`);
+
+}
